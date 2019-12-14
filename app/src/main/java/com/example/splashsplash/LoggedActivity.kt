7d57312efc4ff0183f4a69dc1b1505d621ef01db
ashish kha <ashishkha1000@gmail.com>
2019-12-14 17:43:53 +0530

    firebase login added

--- app/src/main/java/com/example/splashsplash/LoggedActivity.kt
+++ app/src/main/java/com/example/splashsplash/LoggedActivity.kt
@@ -12,9 +12,6 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 
-
-
-
 class LoggedActivity : AppCompatActivity() {
 
     private lateinit var mAuth: FirebaseAuth

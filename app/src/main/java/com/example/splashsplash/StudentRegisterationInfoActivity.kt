711f20077f74096f7f9a8b74b69b5e5d4bdce602
ashish kha <ashishkha1000@gmail.com>
2019-12-21 19:06:36 +0530

    database added

--- app/src/main/java/com/example/splashsplash/StudentRegisterationInfoActivity.kt
+++ app/src/main/java/com/example/splashsplash/StudentRegisterationInfoActivity.kt
@@ -3,9 +3,13 @@ package com.example.splashsplash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_student_registeration_info.*
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import android.util.Log
+
 
 class StudentRegisterationInfoActivity : AppCompatActivity() {
 
@@ -26,6 +30,33 @@ class StudentRegisterationInfoActivity : AppCompatActivity() {
     }
 
     private fun authRetrieve() {
+        val database = FirebaseDatabase.getInstance()
+        var myRef = database.getReference("Users")
 
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val map = dataSnapshot.value as Map<String, Any>
+
+
+                var usernameList = ArrayList<String>()
+
+                map.forEach { (key, value) ->
+                    var userMap = value as Map<String, String>
+                    userMap.forEach { (key, value) ->
+                        Log.d("Tag", "value added in fb 4:")
+                        usernameList.add(value)
+                    }
+                }
+                Log.d("Tag", "value added in fb 3:")
+                studentRegisterationInfoAdapter?.setData(usernameList)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.d("Tag", "value added in fb 2:" + error)
+            }
+        })
     }
 }
--- app/src/main/java/com/example/splashsplash/StudentRegisterationInfoAdapter.kt
+++ app/src/main/java/com/example/splashsplash/StudentRegisterationInfoAdapter.kt
@@ -4,23 +4,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.adapter_student_reg_info.view.*
 
 class StudentRegisterationInfoAdapter :
     RecyclerView.Adapter<StudentRegisterationInfoAdapter.ViewHolder>() {
+
+    private var studentRegisterationInfoAdapterList: List<String>? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.adapter_student_reg_info, parent, false)
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.adapter_student_reg_info, parent, false)
         return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
-        return 5;
+        if (studentRegisterationInfoAdapterList == null) {
+            return 0
+        }
+        return studentRegisterationInfoAdapterList!!.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.txtViewName.setText(studentRegisterationInfoAdapterList?.get(position))
+    }
+
+    fun setData(studentRegisterationInfoAdapterList: List<String>) {
+        this.studentRegisterationInfoAdapterList = studentRegisterationInfoAdapterList
+        notifyDataSetChanged()
+
     }
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        var txtViewName = itemView.txtViewName
     }
 }

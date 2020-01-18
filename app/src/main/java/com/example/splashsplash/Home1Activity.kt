c00a82678b6fc6e8f65c699c4c04f242f54a5a65
ashish kha <ashishkha1000@gmail.com>
2020-01-18 18:06:37 +0530

    comment

--- app/src/main/java/com/example/splashsplash/Home1Activity.kt
+++ app/src/main/java/com/example/splashsplash/Home1Activity.kt
@@ -11,6 +11,7 @@ import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_admin.*
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_home1.*
 
 class Home1Activity : AppCompatActivity() {
 
@@ -19,15 +20,15 @@ class Home1Activity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // get reference to all views
-        var et_user_name = findViewById(R.id.et_user_name) as EditText
+      /*  var et_user_name = findViewById(R.id.et_user_name) as EditText
         var et_password = findViewById(R.id.et_password) as EditText
         var btn_reset = findViewById(R.id.btn_reset) as Button
-        var btn_submit = findViewById(R.id.btn_submit) as Button
+        var btn_submit = findViewById(R.id.btn_submit) as Button*/
 
 
 
 
-        btn_reset.setOnClickListener {
+ /*       btn_reset.setOnClickListener {
             // clearing user_name and password edit text views on reset button click
             et_user_name.setText("")
             et_password.setText("")
@@ -37,11 +38,11 @@ class Home1Activity : AppCompatActivity() {
         btn_submit.setOnClickListener {
             val user_name = et_user_name.text;
             val password = et_password.text;
-            Toast.makeText(this@Home1Activity, user_name, Toast.LENGTH_LONG).show()
+            Toast.makeText(this@Home1Activity, user_name, Toast.LENGTH_LONG).show()*/
 
             // your code to validate the user_name and password combination
             // and verify the same
-            if (user_name.toString().equals("admin") && password.toString().equals("admin")) {
+       /*     if (user_name.toString().equals("admin") && password.toString().equals("admin")) {
                 var intent = Intent(Home1Activity@ this, AdminlogActivity::class.java)
                 startActivity(intent)
 
@@ -49,7 +50,6 @@ class Home1Activity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Login successful", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(applicationContext, "Login failed", Toast.LENGTH_SHORT).show()
-            }
-        }
+            }*/
     }
 }
